f01d8dbae902eaebf25dbba82f940d5ba9174d8e
George Hong <george.r.hong@duke.edu>
2020-12-05 14:38:05 -0700

    Preliminary structure to generate audio from text

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bamBOOK
+
+To install the necessary packages, enter the following in the terminal:   
+`pip install -r requirements.txt`
+
+The output of `python3 first_tts.py` will be an MP3 file.
---
+++ first_tts.py
@@ -0,0 +1,54 @@
+"""Synthesizes speech from the input string of text or ssml.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+#from gtts import gTTS
+from pygame import mixer
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.SynthesisInput(text="It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
+
+# The response's audio_content is binary.
+print(type(response.audio_content))
+
+with open("output.mp3", "wb") as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
+
+# from playsound import playsound
+# playsound('output.mp3')
+# import pygame
+# file = 'output.mp3'
+# pygame.init()
+# pygame.mixer.init()
+# pygame.mixer.music.load(file)
+# pygame.mixer.music.play()
+# pygame.event.wait()
+
+
+# tts = gTTS("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness")
+
+# tts.save('output2.mp3')
