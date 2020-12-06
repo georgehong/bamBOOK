92cbb73439e34c0036bcf746bcf2019a33f3e589
George Hong <george.r.hong@duke.edu>
2020-12-05 23:43:42 -0700

    Added finishing splash screen

--- main.py
+++ main.py
@@ -13,38 +13,44 @@ Config.set('graphics', 'height', 650)
 from kivy.core.window import Window
 # background color
 Window.clearcolor = (.86, .90, .93, 1)
-import random
-from words_for_hangman import RANDOM_WORDS
-from words_for_hangman import ALPHABET_RU
-import re
-
+#import random
+#from words_for_hangman import RANDOM_WORDS
+#from words_for_hangman import ALPHABET_RU
+#import re
+ 
 TRIES = 6
 ALPHABET = [chr(x) for x in range(97, 97 + 26)]
-
+ 
 class Bambook(App):
+    def restart(self, arg):
+        pass
+
+    def exitlevel(self, *args):
+        # obvious
+        App.get_running_app().stop()
+
     def build(self):
         # Window title
         # Words to display
         self.cursor_index = 0
-        self.excerpt = "Elephants are the largest land animals They eat only plants Even though elephants are very strong, they are the only mammals that cannot jump"
+        self.excerpt = "luv u" #Elephants are the largest land animals they eat only plants Even though elephants are very strong, they are the only mammals that cannot jump"
         excerpt_ = self.excerpt.split(" ")
         blanks_ = ['-' * len(x) for x in excerpt_]
         self.final_blanks = " ".join(blanks_)
-
-
-
+        self.final_blank_check = " ".join(blanks_)
+ 
         self.title = 'Bambook'
         mainbox = BoxLayout(orientation='vertical', padding=[6])
         topbox = BoxLayout(orientation='vertical', size_hint=(1, .6))
         toppanel = BoxLayout(orientation='horizontal', size_hint=(1, .2))
-
+ 
         # TODO Add corner stats/info?
         self.center_label = Label(text=self.excerpt, text_size=(800, 300), color=[0, 0, 0, 1])
         #toppanel.add_widget("LEFT")
         toppanel.add_widget(self.center_label)
         #toppanel.add_widget("RIGHT")
-
-        # Typing Progress 
+ 
+        # Typing Progress
         self.progress_text = Label(text=self.final_blanks,
                                   font_size=40, size_hint=(1, .7),
                                   halign='center', valign='center',
@@ -52,10 +58,10 @@ class Bambook(App):
         topbox.add_widget(toppanel)
         topbox.add_widget(self.progress_text)
         mainbox.add_widget(topbox)
-
+ 
         alphabet = GridLayout(cols=7, spacing=[2], size_hint=(1, .4))
         self.alphabet_button = ALPHABET
-
+ 
         for letter in range(0, len(self.alphabet_button) - 5):
             self.alphabet_button[letter] = Button(
                 text=self.alphabet_button[letter],
@@ -64,9 +70,9 @@ class Bambook(App):
                 background_color=[.64, .74, .76, 1],
                 background_normal='')
             alphabet.add_widget(self.alphabet_button[letter])
-
+ 
         #alphabet.add_widget(Widget())
-
+ 
         for letter in range(len(self.alphabet_button) - 5, len(self.alphabet_button)):
             self.alphabet_button[letter] = Button(
                 text=self.alphabet_button[letter],
@@ -75,45 +81,86 @@ class Bambook(App):
                 background_color=[.64, .74, .76, 1],
                 background_normal='')
             alphabet.add_widget(self.alphabet_button[letter])
-        
+       
         self.space_button = Button(
                 text="space",
                 font_size=26,
                 on_press=self.user_letter,
                 background_color=[.64, .74, .76, 1],
                 background_normal='')
-        self.delete_button = Button(
-                text="delete",
-                font_size=26,
-                on_press=self.user_letter,
-                background_color=[.64, .74, .76, 1],
-                background_normal='')
+#        self.delete_button = Button(
+#                text="delete",
+#                font_size=26,
+#                on_press=self.user_letter,
+#                background_color=[.64, .74, .76, 1],
+#                background_normal='')
         alphabet.add_widget(self.space_button)
-        alphabet.add_widget(self.delete_button)
-
+#        alphabet.add_widget(self.delete_button)
+ 
         mainbox.add_widget(alphabet)
 
+        # Restart Splash Screen
+        self.contentpopup = BoxLayout(orientation='vertical',
+                                      padding=[6])
+        self.contentpopuptext = Label(text='You Earned a New Seed!', size_hint=(1, .8))
+        contentpopupbutton = GridLayout(cols=2, spacing=[2], size_hint=(1, .2))
+        self.contentpopupbutton1 = Button(text="Exit",
+                                          background_color=[.64, .74, .76, 1],
+                                          background_normal='',
+                                          on_press=self.exitlevel)
+        self.contentpopupbutton2 = Button(text="Restart",
+                                          background_color=[.64, .74, .76, 1],
+                                          background_normal='',
+                                          on_press=self.restart)
+        contentpopupbutton.add_widget(self.contentpopupbutton1)
+        contentpopupbutton.add_widget(self.contentpopupbutton2)
+        self.contentpopup.add_widget(self.contentpopuptext)
+        self.contentpopup.add_widget(contentpopupbutton)
+        
+        self.popup = Popup(title='Good Job!',
+                           content=self.contentpopup,
+                           size_hint=(None, None), size=(400, 300),
+                           auto_dismiss=False, background_color=(.86, .90, .93, .7),
+                           separator_color=(.86, .90, .93, .7))
+ 
         return mainbox
-    
-
+   
+ 
     def user_letter(self, instance):
         # Get button letter
         self.letter = str(instance.text)
-        # easier access to critical variables
         dex = self.cursor_index
         curr = self.final_blanks
-        # Update displayed text, button text, move cursor
-        self.final_blanks = curr[:dex] + self.letter + curr[dex + 1:]
-        self.progress_text.text = self.final_blanks
-        # To check at every character, look at same index of the above string and self.excerpt and check for a match
-        self.cursor_index += 1
-
+       
+        if self.letter == "space":
+            self.letter = " "
+            self.final_blanks = curr[:dex] + self.letter + curr[dex + 1:]
+            self.progress_text.text = self.final_blanks
+               
+#        if self.letter == "delete":
+#            if dex != 0:
+#                self.final_blanks = curr[:dex -1] + self.final_blank_check[dex:]
+#                self.cursor_index -= 1
+#                dex = self.cursor_index
+        # easier access to critical variables
+        else:
+            # Update displayed text, button text, move cursor
+            self.final_blanks = curr[:dex] + self.letter + curr[dex + 1:]
+            self.progress_text.text = self.final_blanks
+            # To check at every character, look at same index of the above string and self.excerpt and check for a match
+        # make a check to change letter to red
+        if self.letter == self.excerpt[dex].lower():
+            self.cursor_index += 1
+        
+        if self.cursor_index == len(self.excerpt):
+            self.popup.open()
+ 
         # if self.progress_text == self.excerpt:
         #     print("GOOD JOB")
         # else:
         #     print("cHeCk YoUr WoRk")
         #print("HI")
-    
-
+   
+ 
 if __name__ == '__main__':
     Bambook().run()
