b320064b229b1d62acd30e6275f77b8f23b7173b
joyfu-tofu <75565004+joyfu-tofu@users.noreply.github.com>
2020-12-06 05:15:13 -0600

    Delete simplified_tts.py

--- simplified_tts.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-
-tts = gTTS("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness")
-
-tts.save('output2.mp3')
