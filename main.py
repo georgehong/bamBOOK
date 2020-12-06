4f34478a3262605e29af1db41785f55c7361c13f
George Hong <george.r.hong@duke.edu>
2020-12-06 01:38:44 -0700

    sound + button functionality added

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
 from kivy.uix.popup import Popup
 from kivy.config import Config
+from kivy.uix.image import Image
+from kivy.uix.anchorlayout import AnchorLayout
+from playsound import playsound
+from kivy.core.audio import SoundLoader
+from gtts import gTTS
+
 # size window
 Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', 900)
 Config.set('graphics', 'height', 650)
 from kivy.core.window import Window
+
 # background color
 Window.clearcolor = (.86, .90, .93, 1)
 #import random
@@ -20,44 +28,84 @@ Window.clearcolor = (.86, .90, .93, 1)
  
 TRIES = 6
 ALPHABET = [chr(x) for x in range(97, 97 + 26)]
- 
-class Bambook(App):
-    def restart(self, arg):
-        pass
+LEVELS = ["l", "i", "hello world pleased to meet you", "h u", "Elephants are the largest land animals they eat only plants Even though elephants are very strong, they are the only mammals that cannot jump"]
 
-    def exitlevel(self, *args):
+class Bambook(App):
+    def play_audio(value, *args):
+        playsound('output.mp3')
+
+    def exit_level(self, *args):
         # obvious
         App.get_running_app().stop()
+
+    def next_level(self, arg):
+
+        # Increment level counter
+        self.level += 1
+        self.cursor_index = 0
+        # Update text
+        self.excerpt = LEVELS[self.level]
+        excerpt_ = self.excerpt.split(" ")
+        blanks_ = ['-' * len(x) for x in excerpt_]
+
+        self.final_blanks = " ".join(blanks_)
+        self.final_blank_check = " ".join(blanks_)
+
+        self.center_label.text = self.excerpt
+        self.progress_text.text = self.final_blanks
+
+        # Auto playback
+        tts = gTTS(self.excerpt)
+        tts.save('output.mp3')
+        Clock.schedule_once(self.play_audio, 1)
+        self.popup.dismiss()
 
     def build(self):
         # Window title
         # Words to display
         self.cursor_index = 0
-        self.excerpt = "luv u" #Elephants are the largest land animals they eat only plants Even though elephants are very strong, they are the only mammals that cannot jump"
+        # Controls which text excerpt to use
+        self.level = 0
+        self.excerpt = LEVELS[self.level]#"luv u" #Elephants are the largest land animals they eat only plants Even though elephants are very strong, they are the only mammals that cannot jump"
         excerpt_ = self.excerpt.split(" ")
         blanks_ = ['-' * len(x) for x in excerpt_]
         self.final_blanks = " ".join(blanks_)
         self.final_blank_check = " ".join(blanks_)
- 
+
+        tts = gTTS(self.excerpt)
+        tts.save('output.mp3')
+        #sound = SoundLoader.load('output.wav')
+
         self.title = 'Bambook'
         mainbox = BoxLayout(orientation='vertical', padding=[6])
-        topbox = BoxLayout(orientation='vertical', size_hint=(1, .6))
+        middle_panel = BoxLayout(orientation='vertical', size_hint=(1, .6))
         toppanel = BoxLayout(orientation='horizontal', size_hint=(1, .2))
- 
+        bottom_panel = BoxLayout(orientation='horizontal', size_hint=(1, .2))
+
+        #logobox = BoxLayout(orientation='horizontal',)
+        #middle_panel.add_widget(im)
+        #logobox.add_widget(im)
+        #logobox.add_widget(Widget())
+
+        #mainbox.add_widget(im)
         # TODO Add corner stats/info?
-        self.center_label = Label(text=self.excerpt, text_size=(800, 300), color=[0, 0, 0, 1])
-        #toppanel.add_widget("LEFT")
+        #self.center_label = Label(text=self.excerpt, text_size=(800, 300), color=[0, 0, 0, 1], valign='center')
+        self.center_label = Label(text=self.excerpt, text_size=(800, 300), color=[0, 0, 0, 1], valign='center', halign='center')
+        im = Image(source='assets/logo.png')
+        b = Button(text="Replay", on_press=self.play_audio)
+        toppanel.add_widget(im)
         toppanel.add_widget(self.center_label)
-        #toppanel.add_widget("RIGHT")
+        toppanel.add_widget(b)
  
         # Typing Progress
         self.progress_text = Label(text=self.final_blanks,
                                   font_size=40, size_hint=(1, .7),
                                   halign='center', valign='center',
                                   text_size=(800, 300), color=[0, 0, 0, 1])
-        topbox.add_widget(toppanel)
-        topbox.add_widget(self.progress_text)
-        mainbox.add_widget(topbox)
+        middle_panel.add_widget(toppanel)
+        middle_panel.add_widget(self.progress_text)
+        mainbox.add_widget(middle_panel)
+
  
         alphabet = GridLayout(cols=7, spacing=[2], size_hint=(1, .4))
         self.alphabet_button = ALPHABET
@@ -107,11 +155,11 @@ class Bambook(App):
         self.contentpopupbutton1 = Button(text="Exit",
                                           background_color=[.64, .74, .76, 1],
                                           background_normal='',
-                                          on_press=self.exitlevel)
-        self.contentpopupbutton2 = Button(text="Restart",
+                                          on_press=self.exit_level)
+        self.contentpopupbutton2 = Button(text="Next Level",
                                           background_color=[.64, .74, .76, 1],
                                           background_normal='',
-                                          on_press=self.restart)
+                                          on_press=self.next_level)
         contentpopupbutton.add_widget(self.contentpopupbutton1)
         contentpopupbutton.add_widget(self.contentpopupbutton2)
         self.contentpopup.add_widget(self.contentpopuptext)
@@ -122,7 +170,8 @@ class Bambook(App):
                            size_hint=(None, None), size=(400, 300),
                            auto_dismiss=False, background_color=(.86, .90, .93, .7),
                            separator_color=(.86, .90, .93, .7))
- 
+        #sound.play()
+        Clock.schedule_once(self.play_audio, 1)
         return mainbox
    
  
